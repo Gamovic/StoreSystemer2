fe7ce1744a3c4fe73378ab60827a66af9a0d3ec4
Killersoren <skmj@hotmail.dk>
2023-10-23 12:52:34 +0200

    Some client and server stuff

---
+++ Assets/Scripts/ConnectionChecker.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ConnectionChecker : MonoBehaviour
+{
+    private NetworkManager m_NetworkManager;
+
+    private void Start()
+    {
+        m_NetworkManager = GetComponentInParent<NetworkManager>();
+        if (m_NetworkManager != null)
+        {
+            m_NetworkManager.OnClientDisconnectCallback += OnClientDisconnectCallback;
+            m_NetworkManager.ConnectionApprovalCallback = ApprovalCheck;
+            m_NetworkManager.OnClientConnectedCallback += OnClientConnectedCallBack;
+        }
+    }
+
+    private void OnClientConnectedCallBack(ulong obj)
+    {
+
+    }
+
+    private void Setup()
+    {
+        NetworkManager.Singleton.ConnectionApprovalCallback = ApprovalCheck;
+        NetworkManager.Singleton.StartHost();
+    }
+
+    private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
+    {
+        var clientPass = System.Text.Encoding.ASCII.GetString(request.Payload);
+
+        if (clientPass == "room password")
+        {
+            response.CreatePlayerObject = true;
+            print("rifght! passs!!!");
+            response.Approved = true;
+            response.Reason = "correct pass";
+
+        }
+        else
+        {
+            response.Approved = false;
+            print("wroing passs!!!");
+            response.Reason = "incorrect pass";
+        }
+
+    }
+
+    private void OnClientDisconnectCallback(ulong obj)
+    {
+        if (!m_NetworkManager.IsServer && m_NetworkManager.DisconnectReason != string.Empty)
+        {
+            print("and should be here..");
+            Debug.Log($"Approval Declined Reason: {m_NetworkManager.DisconnectReason}");
+        }
+    }
+
+
+}
